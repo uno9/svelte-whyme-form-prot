2cd97def1be8ef00bde1f1cf7b0bfe5a1da7bb81
uno9 <47252405+uno9@users.noreply.github.com>
2021-01-10 18:10:09 +0900

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Button from "./Button.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>What's your Why Me?</h1>
+	<h2>Please tell me about you!</h2>
+	<Button />
+</main>
